57a1721b1a095881b034736c15fd3fa97ab0f8c2
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-02-03 07:33:27 +0000

    [Start]課題８b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
+    
 
   end
 
